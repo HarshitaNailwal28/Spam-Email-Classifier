7904bbccdd8a56e250eb7443104abfe13b04d0d6
HarshitaNailwal28 <110735446+HarshitaNailwal28@users.noreply.github.com>
2023-04-20 13:12:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 python app.py migrate
 pip install numpy scipy scikit-learn
 pip install Scikit-learn
-nltk.downloader stopwords
+pip install nltk.downloader stopwords
