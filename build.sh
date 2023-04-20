7cc764c0dadc5a78cf034970ec4b2788a4d505cd
HarshitaNailwal28 <110735446+HarshitaNailwal28@users.noreply.github.com>
2023-04-20 11:32:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ pip install -r requirements.txt
 python app.py migrate
 pip install numpy scipy scikit-learn
 pip install streamlit
+python -m pip install --upgrade pip
+import nltk 
+nltk.download('popular')
